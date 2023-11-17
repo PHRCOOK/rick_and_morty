df085845a5d163c6c3e52b8281a8eeec73c5d083
PHRCOOK <pabloelleproso@gmail.com>
2023-11-16 21:55:22 -0300

    modif

--- Server/Src/controllers/postUser.js
+++ Server/Src/controllers/postUser.js
@@ -1,19 +1,20 @@
 const { User } = require("../DB_connection");
-
 const postUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
       return res.status(400).send("Faltan datos");
     }
-
-    const user = await User.findOrCreate({
-      where: { email: email, password: password },
+    const [user, created] = await User.findOrCreate({
+      where: { email },
+      defaults: { password },
     });
-    return res.json(user);
+    if (!created) {
+      return res.status(409).send("Usuario ya existe");
+    }
+    return res.status(200).json(user);
   } catch (error) {
-    return res.status(500).json(error.message);
+    res.status(500).json({ error: error.message });
   }
 };
-
 module.exports = postUser;
